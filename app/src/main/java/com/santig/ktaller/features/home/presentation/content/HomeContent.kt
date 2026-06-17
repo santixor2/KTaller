b54a42828dfe23b1a01488300f6a5aac4022166d
Santiago Gimenez <110056571+santixor2@users.noreply.github.com>
2026-06-16 21:19:53 -0300

    configuracion para que la app solo sea en vertical y muestro mensaje cuando la lista esta vacia

--- app/src/main/java/com/santig/ktaller/features/home/presentation/content/HomeContent.kt
+++ app/src/main/java/com/santig/ktaller/features/home/presentation/content/HomeContent.kt
@@ -2,21 +2,27 @@ package com.santig.ktaller.features.home.presentation.content
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.santig.ktaller.core.ui.components.TopBar
 import com.santig.ktaller.features.home.presentation.components.SearchBar
 import com.santig.ktaller.features.home.presentation.components.StatusCardItem
@@ -71,7 +77,23 @@ fun HomeContent(
                     items(7) {
                         StatusCardSkeleton()
                     }
-                } else {
+                }else if (uiState.orders.isEmpty()) {
+                    item {
+                        Box(
+                            modifier = Modifier.fillParentMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(
+                                text = "No hay órdenes para mostrar",
+                                color = Color.White,
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.W500,
+                                textAlign = TextAlign.Center,
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                        }
+                    }
+                }else {
                     items(
                         items = uiState.orders,
                         key = { order -> order.id }
